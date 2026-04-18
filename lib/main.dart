fee0532857c657261fa1452f773b0f6157ec1f7b
EmilianoBarCer <emiliano.barcer@gmail.com>
2026-04-18 00:20:13 -0600

    aqui aprendimos unas estructuras, se hizo la organizacion por carpetass, instalamos la navegacion go_ruter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'core/app_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,18 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'FitTrack',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('FitTrack 🏋️'),
-        ),
-      ),
+      routerConfig: appRouter,
     );
   }
 }
